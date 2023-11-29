1f22c3c4672f33a1bb45d52e64a9c1991ce854af
Mary <kass.o@yandex.ru>
2023-11-29 11:49:44 +0300

    code correction

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
-from src.utils import displaying_statistics_on_cards
-from src.views import outputting_statistics_based_on_data, data_currency_and_share_request, reading_data_from_file, \
+from src.views import displaying_statistics_on_cards
+from src.utils import outputting_statistics_based_on_data, data_currency_and_share_request, reading_data_from_file, \
     writing_data_to_json
 
 data_currency_and_share_request('user_settings.json')
